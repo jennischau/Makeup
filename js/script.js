4185cd917bb19d341b3be698352586e984030e90
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2025-05-14 15:36:51 +0700

    Update story

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ new Swiper('.story-wrapper', {
     }
   })
 
-const stories = document.querySelectorAll('video');
+const stories = document.querySelectorAll('.story-link video');
     
     stories.forEach(video => {
         video.addEventListener('mouseenter', () => {
@@ -40,3 +40,70 @@ toggleButton.addEventListener("click", () => {
     toggleButton.textContent = "Xem thêm";
     }
 });
+
+// Load story
+// Select elements
+const storyItems = document.querySelectorAll('.story-item');
+const modal = document.getElementById('storyModal');
+const modalVideo = modal.querySelector('video');
+const modalTitle = modal.querySelector('.title'); // Sửa từ "modelTitle" thành ".title"
+const closeModal = document.getElementById('closeModal');
+const prevStory = document.getElementById('prevStory');
+const nextStory = document.getElementById('nextStory');
+let currentIndex = -1; 
+// Event listener for story items
+storyItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        currentIndex = index; // Ghi lại index hiện tại
+        openModal(index);
+    });
+});
+function openModal(index) {
+    const item = storyItems[index];
+    const videoSrc = item.getAttribute('data-video');
+    const title = item.querySelector('.story-title');
+
+    modalVideo.src = videoSrc;
+    modalTitle.innerText = title.innerText;
+    modal.style.display = 'flex';
+}
+
+// Navigate to previous video
+prevStory.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex >= 0) {
+        openModal(currentIndex);
+    }else{
+        currentIndex=storyItems.length - 1;
+        openModal(currentIndex);
+    }
+});
+
+// Navigate to next video
+nextStory.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex > storyItems.length - 1) {
+        openModal(0);
+    }else{
+        openModal(currentIndex);
+    }
+});
+
+// Close modal
+closeModal.addEventListener('click', () => {
+    closeModalHandler();
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModalHandler();
+    }
+});
+
+// Close modal and reset video
+function closeModalHandler() {
+    modal.style.display = 'none';
+    modalVideo.pause();
+    modalVideo.src = '';
+    modalTitle.innerText = '';
+}
